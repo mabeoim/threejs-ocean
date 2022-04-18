eaa5044134e1a020754dc2d18bcab0af354e5aa7
mackil5000 <marcusbernroth@gmail.com>
2022-04-18 13:46:06 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import "./style.css";
+import * as three from "three";
+
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new three.WebGLRenderer({
+  canvas: document.querySelector("canvas"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
